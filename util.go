1e631314e64e5259221b5ed65304c56c3dae1dae
Erlend Marius Ommundsen <emommundsen@gmail.com>
2021-10-16 15:50:15 +0200

    Add weekday check + refactor

--- util.go
+++ util.go
@@ -6,20 +6,26 @@ import (
 	"time"
 )
 
-func GetAllCandidates() []string  {
+func GetAllCandidates() []string {
 	return []string{"Hans", "Therese", "Sigurd", "Trym", "Sivert", "Asbjørn"}
 }
 
-func GetCandidate(lastDate time.Time, gap time.Duration) string {
+func IsEnoughTimePassed(lastDate time.Time, gap time.Duration) bool {
 	now := time.Now()
 	diff := now.Sub(lastDate)
-	if diff > gap {
-		rand.Seed(now.UnixNano())
-		allCandidates := GetAllCandidates()
-		i := rand.Intn(len(allCandidates))
-		chosen_candidate := allCandidates[i]
-		log.Printf("Chosen candidate is %s\n", chosen_candidate)
-		return chosen_candidate
-	}
-	return "Trym"
+	return diff > gap
+}
+
+func ItIsMondayMyDudes() bool {
+	return time.Now().Weekday().String() == "Monday"
+}
+
+func GetCandidate() string {
+	now := time.Now()
+	rand.Seed(now.UnixNano())
+	allCandidates := GetAllCandidates()
+	i := rand.Intn(len(allCandidates))
+	chosenCandidate := allCandidates[i]
+	log.Printf("Chosen candidate is %s\n", chosenCandidate)
+	return chosenCandidate
 }
